29977b9103f911c8475eaa87224c39f802170a45
Boris Dimitrov <borisd2000@gmail.com>
2023-02-11 23:27:07 +0200

    create a basic User class

---
+++ user.py
@@ -0,0 +1,6 @@
+# A class for a user of the application
+class User:
+    # Creates a user with a username and password
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
